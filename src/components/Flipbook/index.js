297cb183cbde011239e76f0e6f123cfc95e8aed8
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-08-22 14:25:32 +0100

    typewriter on front page working and in place

--- src/components/Flipbook/index.js
+++ src/components/Flipbook/index.js
@@ -5,14 +5,26 @@ import HTMLFlipBook from "react-pageflip";
 
 function Flipbook(props) {
 	return (
-		<HTMLFlipBook className="flipbook" width={400} height={600}>
-			<div className="demoPage">Page 1</div>
-			<div className="demoPage">Page 2</div>
-			<div className="demoPage">Page 3</div>
-			<div className="demoPage">Page 4</div>
+		<HTMLFlipBook className="flipbook" width={500} height={600}>
+			<div className="frontPage-left">
+				<div className="left-bar"></div>
+				<div className = "typewriter">
+					<div className="wrapper">
+						<p className="line__1">Welcome to my</p>
+						<p className="line__2">photo album!</p>
+					</div>
+				</div>
+			</div>
+
+            
+			<div className="frontPage-right">Page 2</div>
+			<div className="photoPage">Page 3</div>
+			<div className="photoPage">Page 4</div>
 		</HTMLFlipBook>
 	);
 }
 
 
 export default Flipbook
+
+
