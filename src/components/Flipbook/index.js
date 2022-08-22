b07d931c357226a046be96e467290d11f5be8609
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-08-22 15:12:18 +0100

    responsive font for typewriter

--- src/components/Flipbook/index.js
+++ src/components/Flipbook/index.js
@@ -5,10 +5,10 @@ import HTMLFlipBook from "react-pageflip";
 
 function Flipbook(props) {
 	return (
-		<HTMLFlipBook className="flipbook" width={500} height={600}>
+		<HTMLFlipBook className="flipbook" width={400} height={600} size="stretch">
 			<div className="frontPage-left">
 				<div className="left-bar"></div>
-				<div className = "typewriter">
+				<div className="typewriter">
 					<div className="wrapper">
 						<p className="line__1">Welcome to my</p>
 						<p className="line__2">photo album!</p>
@@ -16,10 +16,13 @@ function Flipbook(props) {
 				</div>
 			</div>
 
-            
 			<div className="frontPage-right">Page 2</div>
 			<div className="photoPage">Page 3</div>
 			<div className="photoPage">Page 4</div>
+			<div className="photoPage">Page 5</div>
+			<div className="photoPage">Page 6</div>
+			<div className="photoPage">Page 7</div>
+			<div className="photoPage">Page 8</div>
 		</HTMLFlipBook>
 	);
 }
